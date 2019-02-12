b7806f45093544fe90722c8422b3f618671ac47c
Danny Grove <danny@drgrovellc.com>
2019-02-12 10:51:03 -0800

    Add multi-distro ctags install support

--- install_deps.sh
+++ install_deps.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+SCRIPT_FOLDER=$(dirname -- "$0")
+OS=$(cat /etc/os-release | grep ^NAME | cut -d "=" -f2)
 which ctags-exuberant 2>&1> /dev/null || {
-    sudo apt-get install exuberant-ctags  
+  if [[ "$OS" -eq "Arch Linux" ]]; then
+    pacman -S ctags
+  elif [[ "$OS" -eq "Ubuntu" ]]; then
+    sudo apt-get install exuberant-ctags
+  fi
 }
 
 pip install --user jedi
 cd bundle/completor.vim/
-make js
 npm install -g typescript
+make js
 cd ../../
 echo "let g:completor_tsserver_binary = '$(which tsserver)'" >> completor.vimrc
-echo "let g:completor_node_binary = \"$PWD/bundle/completor.vim/pythonxcompleters/javascript/node_modules/.bin/tern\""
->> completor.vimrc
+echo "let g:completor_node_binary = \"$SCRIPT_FOLDER/bundle/completor.vim/pythonxcompleters/javascript/node_modules/.bin/tern\"" >> completor.vimrc
 echo "let g:completor_python_binary = \"$(which python)\"" >> completor.vimrc
